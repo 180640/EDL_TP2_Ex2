df0766d1453701d62d1d7e3c0172c73421d96631
180640 <louis.detry@student.umons.ac.be>
2020-11-19 16:17:51 +0100

    mlkjhtre

--- test/ProfCoursTest.php
+++ test/ProfCoursTest.php
@@ -329,7 +329,7 @@ class ProfCoursTest extends TestCase
         $val = $cours->updateOne($conn, $idCours);
         $expected_cours_str = $cours->__toString();
         $record_cours = Cours::printOne($conn, $idCours);
-        $this->assertEquals($expected_cours_str, $record_cours->toString(), "Update du cours $idCours ...\n");
+        $this->assertEquals($expected_cours_str, $record_cours->__toString(), "Update du cours $idCours ...\n");
         $this->assertTrue($val, "Update du cours num $idCours ...\n");
        
         
